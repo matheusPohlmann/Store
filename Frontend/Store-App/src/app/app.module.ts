4167ce4ddeee4d8fb2e8cf4ce408becf55b4739e
Matheus Pohlmann <matheus.pohlmann@nutrire.com.br>
2022-04-26 13:51:13 -0300

    interpolação, bindings e diretivas

--- Frontend/Store-App/src/app/app.module.ts
+++ Frontend/Store-App/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdutosComponent
+    ProdutosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/Store-App/src/app/produtos/produtos.component.ts
+++ Frontend/Store-App/src/app/produtos/produtos.component.ts
@@ -8,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosComponent implements OnInit {
 
-  public produtos: any;
+  public produtos: any = [];
+  public produtosFiltereds: any = [];
+  private _filter: string = '';
+
+  public get filter() {
+    return this._filter;
+  }
+
+  public set filter(value: string) {
+    this._filter = value;
+    this.produtosFiltereds = this.filter ? this.filterProducts(this.filter) : this.produtos;
+  }
+
+  filterProducts(filterFor: string): any {
+    filterFor = filterFor.toLocaleLowerCase();
+    return this.produtos.filter(
+      (x: any) => x.name.toLocaleLowerCase().indexOf(filterFor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -19,7 +37,10 @@ export class ProdutosComponent implements OnInit {
   public getProdutos(): void {
 
     this.http.get('https://localhost:5001/api/produtos').subscribe(
-      response => this.produtos = response,
+      response => {
+        this.produtos = response;
+        this.produtosFiltereds = this.produtos;
+      },
       error => console.log(error)
     );
   }
